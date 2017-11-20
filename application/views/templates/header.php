3739858594cff06404c8e79e9afb72f9cd7509b8
Leon <Leon@MacBook-Air-van-Leon.local>
2017-11-20 12:28:09 +0100

    Final bootstrap option added to header. Fixed the menu.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,7 +1,6 @@
 <html>
-		<link rel="stylesheet" href="https://bootswatch.com/flatly/bootstrap.min.css">
-    <link rel="stylesheet" href="https://bootswatch.com/flatly/bootstrap.min.css">
-    <link href="<?php echo base_url(); ?>assets/css/style.css" rel='stylesheet' type='text/css' />
+		<link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
+ 
         <head>
                 <title>Webshop</title>
         </head>
@@ -9,17 +8,16 @@
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      
-
+      <a class="navbar-brand" href="#">DnD Dashboard</a>
     </div>
 
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
+
       <ul class="nav navbar-nav">
-        <li class=""><a href="/">Home <span class="sr-only"></span></a></li>
+        <li class=""><a href="/">Home <span class="sr-only">(current)</span></a></li>
         <li><a href="<?php echo base_url(); ?>contact">Contact</a></li>
-        <li><a href="<?php echo base_url(); ?>users/registration">Registration</a></li>
-        <li><a href="<?php echo base_url(); ?>users/login">Login</a></li>
       <ul class="nav navbar-nav navbar-right">
+        <li><a href="<?php echo base_url(); ?>users/registration">Registratie</a></li>
+        <li><a href="<?php echo base_url(); ?>users/login">Login</a></li>
       </ul>
     </div>
   </div>
