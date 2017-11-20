49aec94465ba2a3f1e38bc8230e54810ac1ba30d
Leon <Leon@MacBook-Air-van-Leon.local>
2017-11-20 12:01:07 +0100

    Added a small functional design in the header.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,6 +1,7 @@
 <html>
 		<link rel="stylesheet" href="https://bootswatch.com/flatly/bootstrap.min.css">
     <link rel="stylesheet" href="https://bootswatch.com/flatly/bootstrap.min.css">
+    <link href="<?php echo base_url(); ?>assets/css/style.css" rel='stylesheet' type='text/css' />
         <head>
                 <title>Webshop</title>
         </head>
@@ -9,12 +10,12 @@
   <div class="container-fluid">
     <div class="navbar-header">
       
-      <a class="navbar-brand" href="#">Webshop</a>
+
     </div>
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
       <ul class="nav navbar-nav">
-        <li class=""><a href="/">Home <span class="sr-only">(current)</span></a></li>
+        <li class=""><a href="/">Home <span class="sr-only"></span></a></li>
         <li><a href="<?php echo base_url(); ?>contact">Contact</a></li>
         <li><a href="<?php echo base_url(); ?>users/registration">Registration</a></li>
         <li><a href="<?php echo base_url(); ?>users/login">Login</a></li>
