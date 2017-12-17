996dbda3ef7563b947ce291874ad64c6274c8776
Armania50 <arya.00767@gmail.com>
2017-12-17 21:13:12 +0100

    Ticket System Last step.
    Ticket System Last step:
    
    - De bij Role (Role_id) wordt bij de bijbehorende User weergegeven zodra er is ingelogd.

--- application/views/users/account.php
+++ application/views/users/account.php
@@ -6,7 +6,23 @@
 <body>
 <div class="container">
     <?php
-    $this->user->getRolesForUserID(array('id'=>$user['id'])); //aanroepen functie NEW!
+    //$this->user->getRolesForUserID(array('id'=>$user['id'])); //aanroepen functie NEW!
+    //var_dump($_SESSION["userRoles"]);
+    foreach ($_SESSION["userRoles"] as $key => $value) {
+        if (!empty($value["role_id"])) { // Is de session niet leeg? Dan dit, anders niks doen.
+            // De bijpassende rol naam pakken uit de database.
+            $this->db->select('name'); // pak het kopje name
+            $this->db->from('roles'); // uit de tabel roles
+
+            $this->db->where('id', $value["role_id"]); // waar id = role id
+            $query = $this->db->get();
+            $roles = $query->result_array();
+            //var_dump($roles);
+            foreach ($roles as $key2 => $value2) {
+                echo "U bent ingelogd als een ".$value2["name"].".<br /><br />";
+            }
+        }
+    }
     ?>
     <h2>User Account</h2>
     <h3>Welcome <?php echo $user['name']; ?>!</h3>
