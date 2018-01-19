661d0d9374786402e0125dd2caf61c985b1f0d6c
Armania50 <arya.00767@gmail.com>
2018-01-19 19:30:12 +0100

    #40, #41, #42
    Winkelwagen gemaakt m.b.v. tutorial. Access systeem zelf toegevoegd.
    
    URL opbouw in progress.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,6 +3,10 @@
  
         <head>
                 <title>Webshop</title>
+                <link href="<?php echo base_url(); ?>assets/css/core.css" media="screen" rel="stylesheet" type="text/css" />
+ 
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery-1.3.2.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/core.js"></script>
         </head>
         <body>
 <nav class="navbar navbar-inverse">
